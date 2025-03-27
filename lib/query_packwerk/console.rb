1fd88a4a6334bff2da237cfd398e148a9c3c4747
Martin Emde <me@martinemde.com>
2025-03-27 13:22:16 -0700

    Better console helpers

--- lib/query_packwerk/console.rb
+++ lib/query_packwerk/console.rb
@@ -1,6 +1,9 @@
 # typed: strict
 # frozen_string_literal: true
 
+require 'irb'
+require 'irb/completion'
+
 module QueryPackwerk
   # Console for QueryPackwerk
   class Console
@@ -38,40 +41,19 @@ module QueryPackwerk
 
         context = Class.new do
           include QueryPackwerk
+          include QueryPackwerk::ConsoleHelpers
           extend T::Sig
-
-          sig { returns(String) }
-          def inspect
-            'query_packwerk console'
-          end
         end
 
-        irb = IRB::Irb.new(IRB::WorkSpace.new(context.new))
+        context_instance = context.new
+        irb = IRB::Irb.new(IRB::WorkSpace.new(context_instance))
         IRB.conf[:MAIN_CONTEXT] = irb.context
+
+        context_instance.welcome
 
         trap('SIGINT') do
           irb.signal_handle
         end
-
-        puts 'QueryPackwerk Console'
-        puts '====================='
-        puts 'Available commands:'
-        puts '  package(pack_name)                         - Get a package by name'
-        puts '  Packages.all                               - Get all packages'
-        puts ''
-        puts 'Package Consumption (how others use this package):'
-        puts '  violations_for(pack_name)                  - Get all violations where others access this package'
-        puts '  violation_sources_for(pack_name)           - Get where others access this package'
-        puts '  violation_counts_for(pack_name)            - Get how often others access this package'
-        puts "  anonymous_violation_sources_for(pack_name) - Get the 'shape' of how others access this package"
-        puts '  anonymous_violation_counts_for(pack_name)  - Get how often each access pattern occurs'
-        puts '  consumers(pack_name)                       - Get which packages consume this package'
-        puts ''
-        puts 'Package Dependencies (how this package uses others):'
-        puts '  todos_for(pack_name)                       - Get all todos where this package accesses others'
-        puts ''
-        puts 'Violations.all                               - Get all violations'
-        puts ''
 
         catch(:IRB_EXIT) do
           irb.eval_input
